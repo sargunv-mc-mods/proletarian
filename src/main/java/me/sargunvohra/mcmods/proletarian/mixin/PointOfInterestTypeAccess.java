80d896355237e8b34c4546705e3cfdef3450d783
Sargun Vohra <sargun.vohra@gmail.com>
2019-04-11 22:09:49 -0700

    update for latest prerelease

---
+++ src/main/java/me/sargunvohra/mcmods/proletarian/mixin/PointOfInterestTypeAccess.java
@@ -0,0 +1,17 @@
+package me.sargunvohra.mcmods.proletarian.mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.village.PointOfInterestType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.Map;
+
+@Mixin(PointOfInterestType.class)
+public interface PointOfInterestTypeAccess {
+
+    @Accessor(value = "field_18849")
+    static Map<BlockState, PointOfInterestType> getStatePoiMap() {
+        throw new IllegalStateException();
+    }
+}
--- src/main/kotlin/me/sargunvohra/mcmods/proletarian/profession/CustomProfessionInit.kt
+++ src/main/kotlin/me/sargunvohra/mcmods/proletarian/profession/CustomProfessionInit.kt
@@ -2,29 +2,24 @@ package me.sargunvohra.mcmods.proletarian.profession
 
 import com.google.common.collect.ImmutableSet
 import me.sargunvohra.mcmods.proletarian.construct
+import me.sargunvohra.mcmods.proletarian.craftingstation.CraftingStationBlock
 import me.sargunvohra.mcmods.proletarian.id
+import me.sargunvohra.mcmods.proletarian.mixin.PointOfInterestTypeAccess
 import net.minecraft.block.Block
 import net.minecraft.item.Item
 import net.minecraft.sound.SoundEvents
-import net.minecraft.tag.BlockTags
-import net.minecraft.tag.Tag
 import net.minecraft.util.registry.Registry
 import net.minecraft.village.PointOfInterestType
 import net.minecraft.village.VillagerProfession
-import kotlin.reflect.full.isSubclassOf
 
 object CustomProfessionInit {
 
     private val poiId = id("proletarian_poi")
     private val professionId = id("proletarian")
 
-    private val poiBlockTag = construct(
-        BlockTags::class.nestedClasses.first { it.isSubclassOf(Tag::class) },
-        poiId
-    )
     private val poiType = construct(
         PointOfInterestType::class,
-        poiId.toString(), poiBlockTag, 1, SoundEvents.ENTITY_VILLAGER_WORK_TOOLSMITH
+        poiId.toString(), ImmutableSet.copyOf(CraftingStationBlock.stateFactory.states), 1, SoundEvents.ENTITY_VILLAGER_WORK_TOOLSMITH
     )
     val profession = construct(
         VillagerProfession::class,
@@ -32,6 +27,9 @@ object CustomProfessionInit {
     )
 
     fun register() {
+        CraftingStationBlock.stateFactory.states.forEach {
+            PointOfInterestTypeAccess.getStatePoiMap()[it] = poiType
+        }
         Registry.register(Registry.POINT_OF_INTEREST_TYPE, poiId, poiType)
         Registry.register(Registry.VILLAGER_PROFESSION, professionId, profession)
     }
